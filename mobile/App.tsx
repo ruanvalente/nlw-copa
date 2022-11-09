3cfaf862aee3ce05614fe5b9b6b3fdcd22b3c32b
ruanvalente <ruanopensuselinux@gmail.com>
2022-11-09 17:38:07 -0300

    fix: fix structure  mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import { NativeBaseProvider, Text, Center } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+
+import { THEME } from "./src/styles/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "./src/components/Loading";
+
+import { SignIn } from "./src/screens/Signin";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
-        <Text color="white" fontSize={24}>
-          NLW - COPA
-        </Text>
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
