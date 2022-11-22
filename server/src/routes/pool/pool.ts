b339aad63d17bd031ded8123203022c8ba6b8276
ruanvalente <ruanopensuselinux@gmail.com>
2022-11-22 11:38:51 -0300

    WIP(backend): create poll with owner id

--- server/src/routes/pool/pool.ts
+++ server/src/routes/pool/pool.ts
@@ -1,27 +1,45 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../../lib/prisma";
 
-import { z } from 'zod'
-import ShortUniqueId from 'short-unique-id';
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 export async function poolRoutes(fastify: FastifyInstance) {
-  fastify.get('/pools/count', async () => {
+  fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
 
-    return { count }
-  })
+    return { count };
+  });
 
-  fastify.post('/pools', async (request: FastifyRequest, reply: FastifyReply) => {
-    const generatedID = new ShortUniqueId({ length: 6 });
-    const createPoolBody = z.object({
-      title: z.string()
-    })
+  fastify.post(
+    "/pools",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const generatedID = new ShortUniqueId({ length: 6 });
+      const createPoolBody = z.object({
+        title: z.string(),
+      });
 
-    const { title } = createPoolBody.parse(request.body)
-    const code = String(generatedID()).toUpperCase()
+      const { title } = createPoolBody.parse(request.body);
+      const code = String(generatedID()).toUpperCase();
 
-    await prisma.pool.create({ data: { title, code } })
-
-    return reply.status(201).send({ code })
-  })
+      try {
+        await request.jwtVerify();
+        await prisma.pool.create({
+          data: {
+            title,
+            code,
+            ownerId: request.user.sub,
+            participant: {
+              create: {
+                userId: request.user.sub,
+              },
+            },
+          },
+        });
+      } catch (error) {
+        await prisma.pool.create({ data: { title, code } });
+      }
+      return reply.status(201).send({ code });
+    }
+  );
 }
