e2947465ece238af3460cec46f3e2694ad5a2045
ruanvalente <ruanopensuselinux@gmail.com>
2022-11-22 11:59:44 -0300

    WIP(backend): create join in poll

--- server/src/routes/pool/pool.ts
+++ server/src/routes/pool/pool.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../lib/prisma";
 
 import { z } from "zod";
 import ShortUniqueId from "short-unique-id";
+import { authenticated } from "../../plugins/authenticated";
 
 export async function poolRoutes(fastify: FastifyInstance) {
   fastify.get("/pools/count", async () => {
@@ -40,6 +41,63 @@ export async function poolRoutes(fastify: FastifyInstance) {
         await prisma.pool.create({ data: { title, code } });
       }
       return reply.status(201).send({ code });
+    }
+  );
+
+  fastify.post(
+    "/pools/:id/join",
+    { onRequest: [authenticated] },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const joinPoolBody = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPoolBody.parse(request.body);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participant: {
+            where: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      });
+
+      if (!pool) {
+        return reply.status(404).send({
+          error: "Poll not found.",
+        });
+      }
+
+      if (pool.participant.length > 0) {
+        return reply.status(400).send({
+          error: "You already joined this poll.",
+        });
+      }
+
+      if (!pool.ownerId) {
+        await prisma.pool.update({
+          where: {
+            id: pool.id,
+          },
+          data: {
+            ownerId: request.user.sub,
+          },
+        });
+      }
+
+      await prisma.participant.create({
+        data: {
+          poolId: pool.id,
+          userId: request.user.sub,
+        },
+      });
+
+      return reply.status(201).send();
     }
   );
 }
