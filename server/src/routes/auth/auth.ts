40a3762ec42dbee9ced1fbadf9529c338d2d0a47
ruanvalente <ruanopensuselinux@gmail.com>
2022-11-22 11:02:15 -0300

    WIP(backend): create token jwt

--- server/src/routes/auth/auth.ts
+++ server/src/routes/auth/auth.ts
@@ -1,11 +1,62 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
 import { prisma } from "../../lib/prisma";
 
 export async function authRoutes(fastify: FastifyInstance) {
-  // fastify.get('/guesses/count', async () => {
-  //   const count = await prisma.guess.count();
+  fastify.post("/users", async (request) => {
+    const createUserBody = z.object({
+      access_token: z.string(),
+    });
 
-  //   return { count }
-  // })
+    const { access_token } = createUserBody.parse(request.body);
+
+    const userResponse = await fetch(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const userData = await userResponse.json();
+
+    const userInfoSchema = z.object({
+      id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      picure: z.string().url().optional()
+    });
+
+    const userInfo = userInfoSchema.parse(userData);
+
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id
+      }
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          name: userInfo.name,
+          email: userInfo.email,
+          avatarUrl: userInfo.picure
+        }
+      })
+    }
+
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl
+    }, {
+      sub: user.id,
+      expiresIn: '7 days'
+    })
+
+    return { token }
+  });
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { poolRoutes } from './routes/pool/pool'
 import { userRoutes } from './routes/user/user'
@@ -14,6 +15,10 @@ async function bootstrap() {
 
   await fastify.register(cors, {
     origin: true
+  })
+
+  await fastify.register(jwt, {
+    secret: 'nlwcopa'
   })
 
   await fastify.register(poolRoutes)
