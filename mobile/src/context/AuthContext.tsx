0165fcd0715563de3bd69a4b60ba40900966c877
ruanvalente <ruanopensuselinux@gmail.com>
2022-11-26 14:11:20 -0300

    WIP(mobile): connect backend for signin

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -3,12 +3,13 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import * as Google from "expo-auth-session/providers/google";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
+import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
-  avatarUrl: string;
+  avatarUrl: string | null;
 }
 
 export interface AuthContextDataProps {
@@ -46,8 +47,24 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     }
   }
 
-  async function signInWithGoogle(accessToken: string) {
-    console.log("token", accessToken);
+  async function signInWithGoogle(access_token: string) {
+    try {
+      setIsUserLoading(true);
+
+      const tokenResponse = await api.post("/users", { access_token });
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${tokenResponse.data.token}`;
+
+      const userInfoResponse = await api.get("/me");
+
+      setUser(userInfoResponse.data.user);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
 
   useEffect(() => {
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,18 @@
+import { Box } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 
+import { SignIn } from "../screens/Signin";
+
 import { AppRoutes } from "./app.routes";
+import { useAuth } from "../hooks/useAuth";
 
 export function Routes() {
+  const { user } = useAuth();
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   );
 }
