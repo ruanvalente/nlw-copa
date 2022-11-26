4accfc21e9c9f1102e202e7776a1c85baaf3013c
ruanvalente <ruanopensuselinux@gmail.com>
2022-11-26 10:29:02 -0300

    WIP(backend): list game details

---
+++ server/src/routes/game/game.ts
@@ -0,0 +1,45 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+import { prisma } from "../../lib/prisma";
+import { authenticated } from "../../plugins/authenticated";
+
+export async function gamesRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    "/pools/:id/games",
+    { onRequest: [authenticated] },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const getPoolParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPoolParams.parse(request.params);
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                poolId: id,
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,33 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 
-import { poolRoutes } from './routes/pool/pool'
-import { userRoutes } from './routes/user/user'
-import { guesseRoutes } from './routes/guesse/guesse'
-import { gamerRoutes } from './routes/gamer/gamer'
-import { authRoutes } from './routes/auth/auth'
+import { poolRoutes } from "./routes/pool/pool";
+import { userRoutes } from "./routes/user/user";
+import { guesseRoutes } from "./routes/guesse/guesse";
+import { gamesRoutes } from "./routes/game/game";
+import { authRoutes } from "./routes/auth/auth";
 
 async function bootstrap() {
   const fastify = Fastify({
-    logger: true
-  })
+    logger: true,
+  });
 
   await fastify.register(cors, {
-    origin: true
-  })
+    origin: true,
+  });
 
   await fastify.register(jwt, {
-    secret: 'nlwcopa'
-  })
+    secret: "nlwcopa",
+  });
 
-  await fastify.register(poolRoutes)
-  await fastify.register(userRoutes)
-  await fastify.register(guesseRoutes)
-  await fastify.register(gamerRoutes)
-  await fastify.register(authRoutes)
+  await fastify.register(poolRoutes);
+  await fastify.register(userRoutes);
+  await fastify.register(guesseRoutes);
+  await fastify.register(gamesRoutes);
+  await fastify.register(authRoutes);
 
-  await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
+  await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ });
 }
 
-bootstrap()
+bootstrap();
